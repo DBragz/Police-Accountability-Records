8df51996e35590c6feecef9937678c0b9c464d8b
dmrbraga <39654677-dmrbraga@users.noreply.replit.com>
2025-02-22 19:26:57 +0000

    Made server port configurable through environment variables

--- server/index.ts
+++ server/index.ts
@@ -47,18 +47,14 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 8000
-  // this serves both the API and the client
-  const port = 8000;
+  // Use environment port or fallback to 8000
+  const port = process.env.PORT || 8000;
   server.listen(port, () => {
     log(`serving on port ${port}`);
   });
