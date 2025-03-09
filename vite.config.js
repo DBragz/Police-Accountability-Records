0cb911041d136669d5baa8eeb3c6742c6e1a859a
DBragz <39654677-DBragz@users.noreply.replit.com>
2025-03-09 20:06:29 +0000

    Restored to 'b8e2480595dd108f5d03700b5b156c609e4584fa'
    Replit-Restored-To: b8e2480595dd108f5d03700b5b156c609e4584fa

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443
-    }
+    port: 5000
   }
 })
